5d589374d792c90c29c229e1e968dd9b62bc6a83
Ernest Joseph <smartlabstech@yahoo.com>
2021-08-04 09:16:29 +0300

    Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
- <header>
-    <div> Covid-19 Tacker</div>
+ <header class="text-center bg-blue-800 text-white p-4 mb-10"> 
+    <div class="text-3xl md:text-5xl font-bold mb-3" >  
+      <i class="fa fa-viruses"></i>Covid-19 Tacker</div>
+
  </header>
 </template>
 
